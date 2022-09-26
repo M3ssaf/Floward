28061b3fb35b98201d9f445be2717b9d337ecde3
mahmoud.a.assaf <mahmoud.a.assaf@gmail.com>
2022-09-27 02:25:29 +0400

    Adding xUnit Testing Project

---
+++ Test.CatalogApi/CatalogProductTest.cs
@@ -0,0 +1,35 @@
+using CatalogBusiness.Business;
+using CatalogBusiness.BusinessContract;
+
+namespace Test.CatalogApi
+{
+    public class CatalogProductTest
+    {
+        #region Declarations
+        private readonly IProductBusiness _productBusiness;
+        #endregion
+
+        #region Constructor
+        public CatalogProductTest(IProductBusiness product)
+        {
+            _productBusiness = product;
+        }
+        #endregion
+        
+        #region Implementation
+        [Fact]
+        public async void GetProduct()
+        {
+            var Result = await _productBusiness.GetProduct(9);
+            Assert.NotNull(Result.Result);
+        }
+
+        [Fact]
+        public async void GeAlltProducts()
+        {
+            var Result = await _productBusiness.GetAllProduct();
+            Assert.NotNull(Result.Result);
+        }
+        #endregion
+    }
+}
---
+++ Test.CatalogApi/Startup.cs
@@ -0,0 +1,23 @@
+﻿using CatalogBusiness.Business;
+using CatalogBusiness.BusinessContract;
+using CatalogDB;
+using CatalogDB.Repo;
+using CatalogDB.RepoContract;
+using Common.Helper;
+using Common.HelperContract;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Test.CatalogApi
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<CatalogDbContext>(options => options.UseSqlServer("Server=LAPTOP-MQA4LEOG\\MSSQLSERVER01;Database=dbCatalog;user id=sa;password=m_01004392068;Trusted_Connection=True;"));
+            services.AddTransient<IProductBusiness, ProductBusiness>();
+            services.AddTransient<IProductRepo, ProductRepo>();
+            services.AddTransient<IRabbitMQHelper, RabbitMQHelper>();
+        }
+    }
+}
